66082946a3a477b60e12ff64438f59eb6addfca9
Ha Qui <havanqui1993@gmail.com>
2021-05-13 00:26:18 +0700

    Complete Todo App

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,24 +2,36 @@ import React, { useState } from 'react';
 import { Container, ListGroup } from 'reactstrap';
 import TodoForm from './TodoForm';
 import TodoItem from './TodoItem';
+import TodoHeader from './TodoHeader';
+
+const initLocalStorage = () =>{
+    return localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : []
+}
 
 const Todo = () => {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(() => initLocalStorage());
 
     const addTodo = todo => {
         const newTodo = [...todos, todo];
         setTodos(newTodo);
-        console.log(todos);
+        localStorage.setItem('todos', JSON.stringify(newTodo));
+    }
+
+    const handleDelete = id =>{
+        const newTodo = todos.filter(x=>x.id != id);
+        setTodos(newTodo);
+        localStorage.setItem('todos', JSON.stringify(newTodo));
     }
 
     return (
         <Container fluid="sm">
             <div className="todo-container">
-                <TodoForm addTodo={addTodo} />
+                <TodoHeader />
+                <TodoForm addTodo={addTodo}/>
                 <ListGroup className="todo-list">
                 {
                     todos.map((todo, key) => 
-                        <TodoItem key={key} item={todo} />
+                        <TodoItem key={key} item={todo} handleDelete={handleDelete} />
                     )
                 }
                 </ListGroup>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import { Badge, ListGroupItem } from 'reactstrap';
+import { Badge, ListGroupItem, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faHome, faTrash } from '@fortawesome/free-solid-svg-icons';
 
-
-function TodoItem({ item }, key) {
-
-    const handleDel = id =>{
-        console.log(id);
-    }
+function TodoItem({ item, handleDelete }, key) {
 
     return (
         <ListGroupItem className="todo-item" key={key}>
             <span>{item.text}</span>
-            <Badge color="dark" onClick={handleDel(item.id)}>
+            <span className="badge badge-dark" onClick={() => handleDelete(item.id)}>
                 <FontAwesomeIcon icon={faTrash} />
-            </Badge>
+            </span>
         </ListGroupItem>
     );
 }
